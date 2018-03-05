038840abf2047eef88f5a49d2d47dafd099eafeb
Hortygp <32112044+hortygp@users.noreply.github.com>
2018-03-05 10:13:27 -0300

    Create estudo.py
    criação de modulos

---
+++ estudo.py
@@ -0,0 +1,18 @@
+@@ -0,0 +1,17 @@
+#coding: utf-8
+#MÓDULO estudo
+
+def f():
+    print("f1")
+    print("f1")
+    print("f1")
+    print("f1")
+    print("f1")
+f()
+
+a =10
+b = "Modulo personalizado"
+
+print("Modulo executado")
+
+print(__name__)
